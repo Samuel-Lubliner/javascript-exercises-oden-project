4e0cb9dde3fc67bd60ffdcf4445c7a5a0c881016
Samuel Lubliner <samlubliner@gmail.com>
2024-01-02 20:46:24 -0600

    pass 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+// # Exercise 03 - Reverse a String
+// Strings in JavaScript cannot be reversed directly
+// split it into something else first
+// do the reversal and then join it back together into a string.
 
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line

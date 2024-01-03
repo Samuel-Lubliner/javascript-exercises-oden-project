d5e09fd9de6ea67ca446614cbaf5d7d106e67f61
Samuel Lubliner <samlubliner@gmail.com>
2024-01-03 00:19:33 -0600

    pass all 12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+// # Exercise 09 - Palindromes
+// Write a function that determines whether or not 
+// a given string is a palindrome.
 
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
